2869392a6fb1d05d4ced2ae001a5aa0b0bc612df
Lysden97 <lysenko.denys97@gmail.com>
2025-01-14 19:32:10 +0100

    initial work

--- website/urls.py
+++ website/urls.py
@@ -17,10 +17,5 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from website.views import IndexView
-
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', IndexView.as_view(), name='index'),
-    path('website/', include('website.urls')),
 ]
