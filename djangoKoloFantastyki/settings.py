d0f0ea9e44f3de9ab1fec1f4e207586ea15c46f3
Lysden97 <lysenko.denys97@gmail.com>
2025-07-08 21:26:19 +0200

    add try to key in settings

--- djangoKoloFantastyki/settings.py
+++ djangoKoloFantastyki/settings.py
@@ -17,16 +17,15 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-try:
-    from .local_settings import *
-except ImportError:
-    pass
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+try:
+    from .local_settings import *
+except ImportError:
+    SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
