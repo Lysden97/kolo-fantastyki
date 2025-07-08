b5ca3b4a8e6eb6edb1941b3848b1e68bb52320a5
Lysden97 <lysenko.denys97@gmail.com>
2025-07-08 21:21:41 +0200

    fix settings key

--- djangoKoloFantastyki/settings.py
+++ djangoKoloFantastyki/settings.py
@@ -26,7 +26,7 @@ except ImportError:
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '${REMOVED_SECRET}'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
