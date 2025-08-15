9d0e16d911bdf2ce91fb6b1240127c92a1461526
Lysden97 <lysenko.denys97@gmail.com>
2025-08-16 01:57:40 +0200

    fixes in tickets

--- website/admin.py
+++ website/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from website.models import Counter
+from website.models import Counter, Ticket, TicketDownloadLog
 
 # Register your models here.
 admin.site.register(Counter)
+admin.site.register(Ticket)
+admin.site.register(TicketDownloadLog)
--- website/models.py
+++ website/models.py
@@ -11,3 +11,15 @@ class Counter(models.Model):
     def __str__(self):
         return self.name
 
+class Ticket(models.Model):
+    ticket_id = models.CharField(max_length=8, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    verified_at = models.DateTimeField(null=True, blank=True)
+    is_used = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'Bilet {self.ticket_id}'
+
+class TicketDownloadLog(models.Model):
+    ip_address = models.GenericIPAddressField()
+    created_at = models.DateTimeField(auto_now_add=True)
