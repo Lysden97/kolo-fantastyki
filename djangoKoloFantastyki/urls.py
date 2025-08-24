8a5c3274e194139c76d1701d66dd666e72cd31da
Lysden97 <lysenko.denys97@gmail.com>
2025-08-24 21:25:10 +0200

    admin fix

--- djangoKoloFantastyki/urls.py
+++ djangoKoloFantastyki/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 from website.views import IndexView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('k9xV7rB2pQzL/', admin.site.urls),
     path('', IndexView.as_view(), name='index'),
     path('website/', include('website.urls')),
 ]
